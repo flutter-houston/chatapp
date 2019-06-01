4653331f33c01c5f538beac9f6e7200098644a02
Binh Tran <binh.tran@us.sogeti.com>
2019-06-01 14:35:22 -0500

    add message submit, message log

--- lib/Views/ChatPage.dart
+++ lib/Views/ChatPage.dart
@@ -1,55 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_app/Views/BasePage.dart';
+import 'package:flutter_app/Views/ChatMessage.dart';
 
 class ChatPage extends BasePage {
 
-  final databaseReference = FirebaseDatabase.instance.reference();
-
   @override
-  Widget build(BuildContext context) {
-    getData();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase Connect'),
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatPage> {
+
+  final databaseReference = FirebaseDatabase.instance.reference();
+  final TextEditingController _chatController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmit(String text) {
+    _chatController.clear();
+      ChatMessage message = new ChatMessage(
+        text: text
+    );
+    setState(() {
+       _messages.insert(0, message);
+    });
+
+  }
+
+  Widget _chatEnvironment (){
+    return IconTheme(
+      data: new IconThemeData(color: Colors.blue),
+          child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal:8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                decoration: new InputDecoration.collapsed(hintText: "Starts typing ..."),
+                controller: _chatController,
+                onSubmitted: _handleSubmit,
+              ),
+            ),
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+
+                onPressed: ()=> _handleSubmit(_chatController.text),
+
+              ),
+            )
+          ],
+        ),
+
       ),
-      body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-
-              RaisedButton(
-                child: Text('Create Record'),
-                onPressed: () {
-                  createRecord();
-                },
-              ),
-
-              RaisedButton(
-                child: Text('View Record'),
-                onPressed: () {
-                  getData();
-                },
-              ),
-              RaisedButton(
-                child: Text('Udate Record'),
-                onPressed: () {
-                  updateData();
-                },
-              ),
-              RaisedButton(
-                child: Text('Delete Record'),
-                onPressed: () {
-                  deleteData();
-                },
-              ),
-            ],
-          )
-      ), //center
     );
   }
 
-  void createRecord(){
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+        children: <Widget>[
+          new Flexible(
+            child: ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(
+            height: 1.0,
+          ),
+          new Container(decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _chatEnvironment(),)
+        ],
+      );
+
+    //   getData();
+    // return Padding(
+    //     padding: EdgeInsets.all(16.0),
+    //     child: Column(
+    //       crossAxisAlignment: CrossAxisAlignment.stretch,
+    //       children: <Widget>[
+    //
+    //         RaisedButton(
+    //           child: Text('Create Record'),
+    //           onPressed: () {
+    //             createRecord();
+    //           },
+    //         ),
+    //
+    //         RaisedButton(
+    //           child: Text('View Record'),
+    //           onPressed: () {
+    //             getData();
+    //           },
+    //         ),
+    //         RaisedButton(
+    //           child: Text('Udate Record'),
+    //           onPressed: () {
+    //             updateData();
+    //           },
+    //         ),
+    //         RaisedButton(
+    //           child: Text('Delete Record'),
+    //           onPressed: () {
+    //             deleteData();
+    //           },
+    //         ),
+    //       ],
+    //     )
+    // );
+
+    void createRecord(){
     databaseReference.child("1").set({
       'title': 'Mastering EJB',
       'description': 'Programming Guide for J2EE'
@@ -73,5 +138,7 @@ class ChatPage extends BasePage {
 
   void deleteData(){
     databaseReference.child('1').remove();
+  }
+
   }
 }
